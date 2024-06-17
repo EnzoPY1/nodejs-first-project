f2c0dd184fa3b1c2662285d87f72b0ff1542a01f
Enzo Durañona <eduranona@bepsa.com.py>
2024-06-17 14:19:23 -0400

    Puerto actualizado

--- src/index.js
+++ src/index.js
@@ -13,6 +13,6 @@ app.use(indexRoutes)
 app.use(express.static(join(__dirname, 'public')))
 
 
-app.listen(3000, () => {
-  console.log('Server started on port 3000')
+app.listen(process.env.PORT || 3000, () => {
+  console.log('Server started on port', process.env.PORT || 3000)
 })
